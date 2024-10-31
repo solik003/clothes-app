1763a17e6301eba62b33e7161e087aeef93165cb
solik003 <malugasola@gmail.com>
2024-10-31 19:46:42 +0200

    add newsletter design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,10 @@ import Announcement from '../components/Announcement'
 import Slider from '../components/Slider'
 import Categories from '../components/Categories'
 import Products from '../components/Products'
+import Newsletter from '../components/Newsletter'
 
 const Home = () => {
+    
   return (
     <div>
         <Announcement />
@@ -13,6 +15,7 @@ const Home = () => {
         <Slider />
         <Categories />
         <Products />
+        <Newsletter />
     </div>
   )
 }
