6f09e11e2a296efe4902a098103f729634d7cefb
solik003 <malugasola@gmail.com>
2024-11-01 13:38:16 +0200

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Login />
     </div>
   );
 }
