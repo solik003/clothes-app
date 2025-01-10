d6ab2edf5c04790007e72a089f5a761e2917123c
solik003 <malugasola@gmail.com>
2025-01-10 20:51:11 +0200

    added categories tabs

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,26 +1,52 @@
 
-import React from 'react';
-import CategoryItem from '../CategoryItem/CategoryItem';
+import React, { useState } from "react";
+import CategoryItem from "../CategoryItem/CategoryItem";
 import { categories } from "../../data";
-import { Stack } from '@mui/material';
+import { Stack, Tabs, Tab, Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-export function Categories () {
+export function Categories() {
+  const [activeTab, setActiveTab] = useState();
+
+  const topCategories = categories.filter((category) => category.isTop);
+
+  const navigate = useNavigate();
+
+  const handleTabChange = (event, newValue) => {
+    setActiveTab(newValue);
+    const category = categories[newValue];
+    navigate(`/products/${category.cat}`);
+  };
+
+
   return (
-    <Stack
-      direction='row'
-      sx={{
-        p: 2,
-        justifyContent: 'space-between',
-        flexWrap: 'wrap',
-        '@media (max-width: 600px)': {
-          p: 0,
-          flexDirection: 'column',
-        },
-      }}
-    >
-      {categories.map((item) => (
-        <CategoryItem item={item} key={item.id} />
-      ))}
-    </Stack>
+    <Box sx={{ width: "100%", p: 2 }}>
+      <Tabs
+        value={activeTab}
+        onChange={handleTabChange}
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="Top Category Tabs"
+        textColor="primary"
+        indicatorColor="primary"
+      >
+        {categories.map((category) => (
+          <Tab key={category.id} label={category.title} />
+        ))}
+      </Tabs>
+
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        gap={{xs: 0, sm: 2}}
+        sx={{
+          justifyContent: "space-between",
+          flexWrap: "wrap",
+        }}
+      >
+        {topCategories.map((item) => (
+          <CategoryItem item={item} key={item.id} />
+        ))}
+      </Stack>
+    </Box>
   );
-};
+}
