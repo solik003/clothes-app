9bafe2c31584835f381aee01bfda7aff094cb2df
solik003 <malugasola@gmail.com>
2025-06-20 14:53:14 +0300

    fix: prevent adding duplicate products to favorites

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,20 +1,20 @@
 
-import React, { useState } from 'react';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardActions from '@mui/material/CardActions';
-import IconButton from '@mui/material/IconButton';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined, Favorite, DeleteOutline } from '@mui/icons-material';
+import {
+  Card, CardHeader, CardMedia, CardActions,
+  IconButton, Typography, Stack
+} from '@mui/material';
+import {
+  FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined,
+  Favorite, DeleteOutline, MoreVert as MoreVertIcon
+} from '@mui/icons-material';
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addFavorite, removeFavorite, removeProduct } from '../../redux/slices/cartRedux';
-import { Typography, Stack } from '@mui/material';
 
 export function Product({ item, isFavoritePage }) {
   const dispatch = useDispatch();
-  const [isFavorited, setIsFavorited] = useState(false);
+  const favorites = useSelector((state) => state.cart.favorites || []);
+  const isFavorited = favorites.some(fav => fav._id === item._id);
 
   if (!item) {
     console.error("Item is undefined");
@@ -27,7 +27,6 @@ export function Product({ item, isFavoritePage }) {
     } else {
       dispatch(addFavorite(item));
     }
-    setIsFavorited(!isFavorited);
   };
 
   const handleRemoveClick = () => {
@@ -38,18 +37,11 @@ export function Product({ item, isFavoritePage }) {
     }
   };
 
-
   return (
     <Card sx={{ width: 340, height: 400, mb: 2, display: 'flex', flexDirection: 'column' }}>
       <CardHeader
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        titleTypographyProps={{
-          noWrap: true
-        }}
+        action={<IconButton aria-label="settings"><MoreVertIcon /></IconButton>}
+        titleTypographyProps={{ noWrap: true }}
         title={item.title}
       />
       <CardMedia
@@ -57,73 +49,42 @@ export function Product({ item, isFavoritePage }) {
         height="220"
         image={item.img?.[0]}
         alt={item.title}
-        sx={{
-          objectFit: 'contain',
-        }}
+        sx={{ objectFit: 'contain' }}
       />
-      <Stack textAlign={'center'} sx={{ flexGrow: 1 }}>
+      <Stack textAlign="center" sx={{ flexGrow: 1 }}>
         {item.salePercentage ? (
           <>
-            <Typography
-              variant="h6"
-              component="div"
-              color='secondary'
-              sx={{
-                textDecoration: 'line-through',
-              }}
-            >
+            <Typography variant="h6" color="secondary" sx={{ textDecoration: 'line-through' }}>
               ${item.price}
             </Typography>
-            <Typography
-              variant="h5"
-              component="span"
-              color="error"
-            >
+            <Typography variant="h5" color="error">
               {item.salePercentage}% OFF
             </Typography>
           </>
         ) : (
-          <Typography
-            variant="h5"
-            component="span"
-          >
+          <Typography variant="h5">
             ${item.price}
           </Typography>
         )}
       </Stack>
       <CardActions
-        disableSpacing={true}
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-        }}
+        disableSpacing
+        sx={{ display: 'flex', justifyContent: 'space-between' }}
       >
-        <IconButton
-          aria-label="add to shopping cart"
-          component={Link}
-          to="/cart"
-        >
+        <IconButton aria-label="add to cart" component={Link} to="/cart">
           <ShoppingCartOutlined />
         </IconButton>
-        <IconButton
-          aria-label="view details"
-          component={Link}
-          to={`/product/${item._id}`}
-        >
+        <IconButton aria-label="view details" component={Link} to={`/product/${item._id}`}>
           <SearchOutlined />
         </IconButton>
         <IconButton
           onClick={isFavoritePage ? handleRemoveClick : handleClick}
-          aria-label={isFavoritePage ? "remove from favorites" : "add to favorites"}
+          aria-label={isFavoritePage ? "remove from favorites" : "toggle favorite"}
         >
           {isFavoritePage ? (
             <DeleteOutline />
           ) : (
-            isFavorited ? (
-              <Favorite color='primary' />
-            ) : (
-              <FavoriteBorderOutlined />
-            )
+            isFavorited ? <Favorite color="primary" /> : <FavoriteBorderOutlined />
           )}
         </IconButton>
       </CardActions>
