44e175ae7fe1de3d4c76d432b8db9ecc41c0dd49
solik003 <malugasola@gmail.com>
2025-07-01 17:42:47 +0300

    add selectors

--- src/redux/selectors/productsSelectors.js
+++ src/redux/selectors/productsSelectors.js
@@ -1,2 +1,8 @@
 
 export const getProducts = (state) => state.cart.products;
+
+export const selectError = (state) => state.products.error;
+
+export const selectProducts = (state) => state.products.products;
+export const selectFilteredProducts = (state) => state.products.filteredProducts;
+export const selectLoading = (state) => state.products.loading;
